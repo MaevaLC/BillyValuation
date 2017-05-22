71c743fa85a3a8885559b15c3e7a1017cb4b02fe
Maëva LE CLECH <m.leclech@estia.local>
2017-05-22 20:13:45 +0200

    Complete overhaul of Tree.py
    Added the lib graphics.py

--- Tree.py
+++ Tree.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue May 16 09:02:22 2017
+Created on Mon May 22 16:43:57 2017
 
 @author: m.leclech
 """
@@ -11,133 +11,156 @@ import json
 from graphics import Point, Text, GraphWin
 
 
-global compteur
-
-
-def fillListParent(pathFile):
-    """Filling the list Parent with the data from the file -> [2] in a int with the index of the Parent"""    
+class Word:
+    """Class to define a word, by its lemme, position, parent, children"""
     
-    #list where dependency is done by specifying its parents (Google way)
-    listeDependencyParent =[]    
+    def __init__(self, lemme, position, parent):
+        """Constructor"""
+    
+        self.lemme = lemme
+        self.position = position
+        self.parent = parent
+        self.children = []
+        
+
+def createListWords(pathFile):
+    """Create a list of objects (Word)
+    
+    Args: 
+        the path of the annotated data file (string)
+    Returns: 
+        a list containing Word objects (list)
+    
+    """
+    
+    listWords = []
     with open(pathFile, "r") as f:
         annotatedText = json.load(f)
         for i in range(len(annotatedText["tokens"])):
             annotatedWord= annotatedText["tokens"][i]
             dependency = (annotatedWord["dependencyEdge"]["headTokenIndex"])
             text = (annotatedWord["text"]["content"])
-            listeDependencyParent.append([text,i,dependency])
-    return listeDependencyParent
+            listWords.append(Word(text, i, dependency))
+    for word in listWords:
+        listWords[word.parent].children.append(word.position)
+    return listWords
     
 
-def fillListChildren(listeDependencyParent):
-    """Reversing the data to make the list Children -> now [2] is a list with the index of children ="""    
+def findRootIndex(listWords):
+    """Find the root's index
     
-    #list where dependency is done by specifying its children (the way my Nodes work)
-    listeDependencyChildren =[]
-    for i in range(len(listeDependencyParent)):
-        listeEnfant = []
-        for j in range(len(listeDependencyParent)):
-            if listeDependencyParent[i][1] == listeDependencyParent[j][2] :
-                listeEnfant.append(j)
-        listeDependencyChildren.append([listeDependencyParent[i][0],listeDependencyParent[i][1],listeEnfant])
-    return listeDependencyChildren
+    Args:
+        a list of words (list of Word objects)
+    Returns:
+        the position of the root in the sentence (int)
     
+    """
+    
+    for word in listWords :
+        if word.position in word.children :
+            return word.position
 
-class Node:
-    """Class to define a node in the tree"""
+
+def countOfParents(count, listWords, wordIndex):
+    """Find the number of parent a word have
     
-    def __init__(self, text, position, lemme, pos, dependencyEdge, childPosition):
-        """Constructor"""
+    Args:
+        the list of words (list of Word objects)
+        the position of the word (int)
+    Returns:
+        the numer of parents (int)
+     
+    """
+    
+    word = listWords[wordIndex]
+    while (word.position != word.parent):
+        count += 1        
+        word = listWords[word.parent]
+    return count
+
+
+def setupRoot(board, length, height, listWords):
+    """Draw the root and add it to the listWordOnBoard
+    
+    Args:
+        the board which will be use to draw on (board, from graphics)
+        length : the length parameter of the anchor for the root (int)
+        heigth : the height parameter of the anchor for the root (int)
+        listWords : the list of Words to be printed (list of Word objects)
+    Returns: 
+        the list of the words on the board (list of Word objects)
+         
+    """   
+    
+    listWordOnBoard = []
+    root = listWords[findRootIndex(listWords)]
+    rootDrawing = Text(Point(length,height),root.lemme)
+    rootDrawing.draw(board)
+    board.getMouse()  #can get annotated, it just to make a pause
+    listWordOnBoard.append(root)
+    return listWordOnBoard
+            
+
+def setupRestOfWords(board, length, height, listWordOnBoard, listWords):
+    """While every word in not in place on the board, continue to place them
+    
+    Args:
+        the board which will be use to draw on (board, from graphics)
+        length : the length parameter of the anchor for the root (int)
+        heigth : the height parameter of the anchor for the root (int)
+        listWordOnBoard : the list of the words on the board (list of Word objects)
+        listWords : the list of Words to be printed (list of Word objects)
+    Returns: 
+        none
         
-        self.text = text
-        self.position = position
-        self.lemme = lemme
-        self.pos = pos
-        self.dependencyEdge = dependencyEdge
-        self.childPosition = childPosition
- 
+    """
 
-def createListNode(listeDependencyChildren):
-    """List of Node made with the listeDependencyChildren
-    
-    listeDependencyChildren does not have all the info required by the constructor yet, will be added later if needed"""    
-    
-    listeNode = []
-    for i in range(len(listeDependencyChildren)) :
-        listeNode.append(Node(listeDependencyChildren[i][0],listeDependencyChildren[i][1],"none","none","none",listeDependencyChildren[i][2]))
-    return listeNode
+    wordOnBoardCheckedCount = 1        
+    while len(listWordOnBoard)<len(listWords):
+        word = listWordOnBoard[wordOnBoardCheckedCount-1]
+        for childIndex in word.children:
+            #since the root refer to itself as his own child
+            #check to not make it duplicate to infinite
+            if childIndex != word.parent:
+                childWord = listWords[childIndex]
+                lengthAnchor = length + 75*childIndex
+                count = 0  
+                heightAnchor = height + countOfParents(count, listWords, childIndex)*50
+                anchor =  Point(lengthAnchor, heightAnchor)
+                Text(anchor, childWord.lemme).draw(board) #print the word on the board
+                listWordOnBoard.append(childWord) #add the word to the listWordOnBoard
+                board.getMouse()  #can get annotated, it just to make a pause    
+            else :                              
+                pass
+        wordOnBoardCheckedCount += 1
+    board.getMouse()  # pause, wait for a click on the window
 
-
-def findRootIndex(listeDependencyChildren):
-    """Find the root's index"""
-    
-    for i in listeDependencyChildren :
-        if i[1] in i[2] :
-            return i[1]
-
-
-def countOfParent(listeDependencyParent, indexWord):
-    """Find the number of parent a word have, given the list of all dependency"""
-    
-    global compteur
-    motEnfant = listeDependencyParent[indexWord]
-    if (motEnfant[1] != motEnfant[2]):
-        compteur+=1        
-        countOfParent(listeDependencyParent, motEnfant[2])    
-    return compteur
-
-
-def setupRoot(board, longueur, hauteur, listeDependencyChildren):
-    """Find the root, draw it and add it to the listMotPlace"""   
-    
-    listeMotPlace=[]
-    for i in listeDependencyChildren :
-        if i[1] in i[2] :
-            root = Text(Point(longueur,hauteur),i[0])
-            root.draw(board)
-            board.getMouse()  #can get annotated, it just to make a pause
-            listeMotPlace.append(i)
-            return listeMotPlace
-
-
-def setupRestOfWords(board, longueur, hauteur, listeMotPlace, listeDependencyChildren, listeDependencyParent):
-    """While every word in not in place on the board, continue to place them"""
-    
-    global compteur
-    i = 1        
-    while len(listeMotPlace)<len(listeDependencyChildren):
-        mot = listeMotPlace[i-1]
-        for j in mot[2]:
-            if j == listeMotPlace[0][2][0] :
-                pass    #since the root refer to itself as children, small trick to not make it duplicate to infinite
-            else :
-                compteur = 0                
-                motEnfant = listeDependencyChildren[j]
-                Text(Point(longueur+75*j,hauteur+countOfParent(listeDependencyParent,j)*50),motEnfant[0]).draw(board) #print the word on the board, position need to be adjusted
-                listeMotPlace.append(listeDependencyChildren[j]) #add the word to the listMotPlace
-                board.getMouse()    #can get annotated, it just to make a pause
-        i += 1
-    board.getMouse() # pause, wait for a click on the window
-    board.close()  #board get closed properly
-    
 
 def drawTree(pathFile):
-    """Draw the tree of the annotatedFile provided"""
+    """Draw the tree of the annotatedFile provided
     
-    global longueur
-    global hauteur
-    listeParent = fillListParent(pathFile)
-    listeChildren = fillListChildren(listeParent)    
+    Args:
+        the path of the annotated data file (string)
+    Returns:
+        a board with the words printed on it (board, from graphics)
+        
+    """
+    
+    listWords = createListWords(pathFile)
     #definition of the Board to print the tree
     lengthBoard = 1500
     heightBoard = 700
     board = GraphWin("My Board", lengthBoard, heightBoard)
     #def of my anchor for the first point
-    longueur = (findRootIndex(listeChildren)/len(listeChildren)) * lengthBoard + 100
-    hauteur = (findRootIndex(listeChildren)/len(listeChildren)) * heightBoard + 100
-    #place the words
-    liste = setupRoot(board, longueur, hauteur, listeChildren)
-    setupRestOfWords(board, longueur, hauteur, liste, listeChildren, listeParent)
-    
-#test
+    length = (findRootIndex(listWords)/len(listWords)) * lengthBoard + 100
+    height = (findRootIndex(listWords)/len(listWords)) * heightBoard + 100
+    #place the words on the board
+    listWordOnBoard = setupRoot(board, length, height, listWords)
+    setupRestOfWords(board, length, height, listWordOnBoard, listWords)
+    #pause, wait for a click on the window, then close properly
+    board.getMouse() 
+    board.close()
+   
+ 
+#test  
 #drawTree('annotatedText.json')
