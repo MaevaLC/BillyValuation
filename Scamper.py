5e44a93f6856bdc721878b62f182b7fd5e61e5de
Maëva LE CLECH <m.leclech@estia.local>
2017-05-30 21:55:48 +0200

    Test of diff libs for neural network

---
+++ Scamper.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 29 13:53:38 2017
+
+@author: m.leclech
+"""
+
+
+import json
+import os
+
+
+
+url = "neptune2.estia.fr"
+seance = 2
+
+
+
+def eliminate(jsonFile):
+    with open(jsonFile) as f:
+        annotatedText = json.load(f)
+        for word in annotatedText["tokens"]:
+                if word["dependencyEdge"]["label"] == "AMOD":
+                    annotatedText["tokens"].remove(word)
+        return annotatedText["tokens"]
+        
+
+files = []
+for element in os.listdir("annotatedText/"+url+"/"+str(seance)):
+    if element.endswith('.json'):
+        files.append(element)
+
+for file in files:    
+    with open("annotatedText/"+url+"/"+str(seance)+"/"+file) as f:
+        annotatedText = json.load(f)
+    sentence = ""
+    for word in annotatedText["tokens"]:
+        sentence += word["text"]["content"] + " "
+    print(sentence)    
+    e = eliminate("annotatedText/"+url+"/"+str(seance)+"/"+file)
+    sentenceE =""
+    for word in e:
+        sentenceE += word["text"]["content"] + " "
+    print (sentenceE)
+    print("")
+        
